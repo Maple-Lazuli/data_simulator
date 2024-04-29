9ef172a4a78a25582117d33fa0863b68555dd089
Ada <the.nostra.tymus@gmail.com>
2024-04-29 16:45:54 -0400

    added a different default repr and added a missing space

--- data_simulator/format.py
+++ data_simulator/format.py
@@ -16,20 +16,22 @@ class Format:
         self.str_buffer = None
 
     def get_bytes(self):
-        return bytes(self.data, 'utf-8')
+        pass
 
     def get_hex_dump(self):
-        byte_array = bytes(self.data, 'utf-8')
-        return hex_dump(byte_array, self.endianess)
+        pass
 
     def __and__(self, other):
-        return HexLink(self.get_hex_dump() + other.get_hex_dump())
+        return HexLink(self.get_hex_dump() + " " + other.get_hex_dump())
 
     def __or__(self, other):
         return ByteLink(self.get_bytes() + other.get_bytes())
 
     def __add__(self, other):
-        return Link(self.get_hex_dump() + other.get_hex_dump(), self.get_bytes() + self.get_bytes())
+        return Link(self.get_hex_dump() + " " + other.get_hex_dump(), self.get_bytes() + self.get_bytes())
 
     def __str__(self):
+        return self.get_hex_dump()
+
+    def __repr__(self):
         return self.get_hex_dump()
